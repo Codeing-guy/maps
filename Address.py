e9c239ba10b2b761009dbaae4100a336f5ba278a
coderamit <59659828+Codeing-guy@users.noreply.github.com>
2020-02-25 21:32:23 +0530

    Update Address.py

--- Address.py
+++ Address.py
@@ -1,12 +1,10 @@
 import webbrowser,sys,pyperclip
 
-sys.argv #['mapit.py','ring','road','jalgaon']
+sys.argv 
 if len(sys.argv) >1:
-      #['mapit.py','ring','road','jalgaon']>> ['ring road jalgaon'   ]
       address=' '.join(sys.argv[1:])
       print(address)
 else:
      address=pyperclip.paste()
-        
-#https://www.google.com/maps/place/<Address>
+       
 webbrowser.open('https://www.google.com/maps/place/'+ address)
